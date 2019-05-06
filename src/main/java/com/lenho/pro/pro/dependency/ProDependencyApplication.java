92d92fa74c32b21826f418c21126b3e6c13070eb
langyonghe <langyh@uooguo.com>
2019-05-06 11:29:21 +0800

    首次提交

---
+++ src/main/java/com/lenho/pro/pro/dependency/ProDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.lenho.pro.pro.dependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProDependencyApplication.class, args);
+    }
+
+}
